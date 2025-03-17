df2e3dc1106cd890c1d2fb8a318a40fe0338c823
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2025-03-17 21:59:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 7093899037))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Mrshubh_1227")
+OWNER_ID = int(getenv("OWNER_ID", 5099049612))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/arame9")
